e4c9f957030126d02bb754acbc0c5a9dd610b750
abhi302005 <abhi302005@gmail.com>
2025-01-13 00:26:22 +0530

    feat+refactor: add proper comments | create new functions - _isBucketExists : check is default bucket exits - uploadFile: upload the single file - uploadMultipleFiles: upload multiple files at once - createBucket: create new bucket

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+import fs from "fs"
 import * as S3 from "@aws-sdk/client-s3"
+import { PutObjectMethodOptions, ACLString } from "./utils.js"
 
+// #### START-SECTION: Error-Codes ####################
+// Description:
 const ERROR_CODES = {
   UPLOAD_FILE_ERROR: "UPLOAD_FILE_ERROR",
   READ_FILE_ERROR: "READ_FILE_ERROR",
   GET_FILE_INFO_ERROR: "GET_FILE_INFO_ERROR",
   DELETE_FILE_ERROR: "DELETE_FILE_ERROR",
+  CREATE_BUCKET_ERROR: "CREATE_BUCKET_ERROR",
 }
-export class S3Service {
-  constructor(bucketName, region, credentials) {
-    this._bucketName = bucketName
+// #### END-SECTION: Error-Codes ####################
 
+// #### START-SECTION: class S3Service ####################
+// Description: ready-to-go functions to use S3 service
+export class S3Service {
+  constructor(region, credentials, defaultBucketName = null) {
+    this._bucketName = defaultBucketName
+    this._region = region
     this._s3 = new S3.S3Client({
       region: region,
       credentials: {
@@ -19,7 +28,9 @@ export class S3Service {
       },
     })
   }
-
+  _isBucketExists() {
+    if (this._bucketName === null) throw new Error("bucket not found!")
+  }
   async _streamToString(stream) {
     return new Promise((resolve, reject) => {
       const chunks = []
@@ -29,9 +40,12 @@ export class S3Service {
     })
   }
 
+  // #### START-SECTION: addData() ####################
+  // Description: function to add data in json format
   async addData(payload, s3path, fileName) {
     try {
       //code
+      _isBucketExists()
       if (!payload || !s3path || !fileName) {
         throw new Error(
           "Missing required inputs: payload, s3path, or fileName."
@@ -49,7 +63,7 @@ export class S3Service {
         Bucket: this._bucketName,
         Key: s3Key,
         Body: JSON.stringify(payload),
-        ContentType: ContentType,
+        ContentType: ContentType, // bug : it's wrong | may give error
       }
       const command = new S3.PutObjectCommand(s3Params)
       const result = await this._s3.send(command)
@@ -68,9 +82,14 @@ export class S3Service {
     }
   }
 
+  // #### END-SECTION: addData() ####################
+
+  // #### START-SECTION: readData() ####################
+  // Description: function to read-data
   async readData(s3path) {
     try {
       //code
+      _isBucketExists()
       if (!s3path) {
         throw new Error("Missing required input: s3path")
       }
@@ -94,10 +113,14 @@ export class S3Service {
       throw errObj
     }
   }
+  // #### END-SECTION: readData() ####################
 
+  // #### START-SECTION: deleteFile() ####################
+  // Description: delete a particular file using file-path
   async deleteFile(s3path) {
     try {
       //code
+      _isBucketExists()
       if (!s3path) {
         throw new Error("Missing required input: s3path")
       }
@@ -121,9 +144,13 @@ export class S3Service {
       throw errObj
     }
   }
+  // #### END-SECTION: deleteFile() ####################
 
+  // #### START-SECTION: getFileInfo() ####################
+  // Description: get file info using file-path
   async getFileInfo(s3path) {
     try {
+      _isBucketExists()
       if (!s3path) {
         throw new Error("Missing required inputs: s3path")
       }
@@ -147,4 +174,120 @@ export class S3Service {
       throw errObj
     }
   }
+  // #### END-SECTION: getFileInfo() ####################
+
+  // #### START-SECTION: uploadFile() ####################
+  // Description: upload single file on s3 // testingInProgress : true;
+  async uploadFile(file, s3Path, bucketName = this._bucketName, options = {}) {
+    try {
+      //code
+      _isBucketExists()
+      if (!file || typeof file !== "object" || s3Path)
+        throw new Error("file or s3Path missing!")
+      const metadata = Object.assign({}, options?.metadata)
+      metadata["content-type"] =
+        metadata["content-type"] ?? "application/octet-stream"
+
+      const s3Key = `${s3Path}/${file?.originalname}`
+
+      const s3Params = Object.assign(
+        {},
+        {
+          BucketName: bucketName ?? this._bucketName,
+          Body: fs.createReadStream(file?.path),
+          Key: s3Path,
+        },
+        PutObjectMethodOptions({ metadata })
+      )
+      const command = new S3.PutObjectCommand(s3Params)
+      const result = await this._s3.send(command)
+      return { ...result, s3Key, success: true }
+    } catch (error) {
+      const errObj = {
+        success: false,
+        message: error.message || ERROR_CODES.UPLOAD_FILE_ERROR,
+        code: error.code || "NOT_FOUND!",
+        stack: error.stack || [],
+      }
+      console.log(`ERROR @ uploadFile`, errObj)
+      delete errObj.stack
+      delete errObj.code
+      throw errObj
+      // return errObj
+    }
+  }
+  // #### END-SECTION: uploadFile() ####################
+
+  // #### START-SECTION: uploadMultipleFIles() ####################
+  // Description: upload multiple files in a folder // testingInProgress : true;
+  async uploadMultipleFiles(
+    files,
+    s3Path,
+    bucketName = this._bucketName,
+    options = {}
+  ) {
+    try {
+      //code
+      _isBucketExists()
+      if (!files || !Array.isArray(files) || !s3Path)
+        throw new Error("missing files or s3Path")
+      const respArr = new Array()
+      await Promise.all(
+        files.map(async (file) => {
+          const resp = this.uploadFile(file, s3Path, bucketName, options)
+          respArr.push(resp)
+        })
+      )
+      return respArr
+    } catch (error) {
+      const errObj = {
+        success: false,
+        message: error.message || ERROR_CODES.UPLOAD_FILE_ERROR,
+        code: error.code || "NOT_FOUND!",
+        stack: error.stack || [],
+      }
+      console.log(`ERROR @ uploadMultipleFiles`, errObj)
+      delete errObj.stack
+      delete errObj.code
+      throw errObj
+      // return errObj
+    }
+  }
+  // #### END-SECTION: uploadMultipleFIles() ####################
+
+  // #### START-SECTION: createBucket() ####################
+  // Description: create new bucket // testingInProgress : true;
+  async createBucket(bucketName, access = "private", isDefault = false) {
+    try {
+      //code
+      if (!bucketName) throw new Error("bucketName missing")
+      const s3Params = {
+        Bucket: bucketName,
+        CreateBucketConfiguration: {
+          LocationConstraint: this._region,
+        },
+        ACL: ACLString(access),
+      }
+      const command = new S3.CreateBucketCommand(s3Params)
+      const result = this._s3.send(command)
+      if (isDefault) {
+        this._bucketName = bucketName
+      }
+      return { ...result, success: true }
+    } catch (error) {
+      const errObj = {
+        success: false,
+        message: error.message || ERROR_CODES.CREATE_BUCKET_ERROR,
+        code: error.code || "NOT_FOUND!",
+        stack: error.stack || [],
+      }
+      console.log(`ERROR @ `, errObj)
+      delete errObj.stack
+      delete errObj.code
+      throw errObj
+      // return errObj
+    }
+  }
+  // #### END-SECTION: createBucket() ####################
 }
+// #### END-SECTION: class S3Service ####################
